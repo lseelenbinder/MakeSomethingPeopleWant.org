ac40be0e59496c309462fc0fc37f298a4a8f5861
Luke Seelenbinder <luke.seelenbinder@gmail.com>
2011-05-12 22:11:47 -0400

    Add extension icon; bump version to 1.1.

--- chrome_extension/makesomething/app.js
+++ chrome_extension/makesomething/app.js
@@ -3,13 +3,17 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (changeInfo.url != null) {
         var domains = localStorage["domains"];
         var blocked_domains = domains.split("\n");
-        for (i = 0; i < blocked_domains.length; i++) {
-            domain = blocked_domains[i].replace("*", ".*");
-            var re = new RegExp(domain, ["i"]);
-            if (re.test(changeInfo.url)) {
-                var url = "http://makesomethingpeoplewant.s3-website-us-east-1.amazonaws.com/";
-                chrome.tabs.update(tabId, {url:url});
-                break;
+        if (blocked_domains.length != 0) {
+            for (i = 0; i < blocked_domains.length; i++) {
+                domain = blocked_domains[i].trim().replace("*", ".*");
+                if (domain.length != 0) {
+                    var re = new RegExp(domain, ["i"]);
+                    if (re.test(changeInfo.url)) {
+                        var url = "http://makesomethingpeoplewant.s3-website-us-east-1.amazonaws.com/";
+                        chrome.tabs.update(tabId, {url:url});
+                        break;
+                    }   
+                }
             }
         }
     }
